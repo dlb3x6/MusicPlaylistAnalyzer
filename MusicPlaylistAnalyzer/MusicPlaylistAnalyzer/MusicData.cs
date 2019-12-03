31bfca16c41fdee4ef3c88e887d71477f583f8d5
dlb3x6 <57881380+dlb3x6@users.noreply.github.com>
2019-12-03 13:19:25 -0600

    Added files correctly

---
+++ MusicPlaylistAnalyzer/MusicPlaylistAnalyzer/MusicData.cs
@@ -0,0 +1,20 @@
+﻿namespace MusicPlaylistAnalyzer
+{
+    public class MusicData
+    {
+        public string Name, Artist, Album, Genre;
+        public int Size, Time, Year, Plays;
+
+        public MusicData (string name, string artist, string album, string genre, int size, int time, int year, int plays)
+        {
+            Name = name;
+            Artist = artist;
+            Album = album;
+            Genre = genre;
+            Size = size;
+            Time = time;
+            Year = year;
+            Plays = plays;
+        }
+    }
+}
